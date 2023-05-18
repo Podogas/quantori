286c6e5140ac28a1026277b8558aedc67c917870
Alexandr Podogas <a.podogas@gmail.com>
2023-05-18 18:42:15 +0400

    add main page

--- final-task/src/components/Main/Main.tsx
+++ final-task/src/components/Main/Main.tsx
@@ -4,9 +4,7 @@ const Main = () => {
 
     return(
         <main className="main">
-            <h1 className="main__title">Q-1 Search</h1>
-            <h3 className="main__subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt u</h3>
-            <button className="main__button" type="button">Login</button>
+
         </main>
     )
 }
