69291bffe4b68c167e8f205ab7437bd3ba138070
Alexandr Podogas <a.podogas@gmail.com>
2023-05-19 22:32:15 +0400

    add validation for signUp and login forms

--- final-task/src/components/LoginForm/LoginForm.tsx
+++ final-task/src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,96 @@
 import './LoginForm.css';
+import {useRef, useState} from 'react';
 
 const LoginForm = ({setFormToShow}:{setFormToShow:(string:string)=>void}) => {
 
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passInputRef = useRef<HTMLInputElement>(null);
+    const [isEmailInputValid, setIsEmailInputValid] = useState(false);
+    const [isPassInputValid, setIsPassInputValid] = useState(false);
+    const [isEmailErrorShown, setIsEmailErrorShown] = useState(false);
+    const [isPassErrorShown, setIsPassErrorShown] = useState(false);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+    // TODO mb create functions that returns related error messages. Refactor ugly ternary operators in jsx.
+    const login = () => {
+        console.log('LOGIN')
+    }
+    const validateEmail = () => {
+        if(emailInputRef.current){
+            const emailValue = emailInputRef.current.value;
+            if(emailRegex.test(emailValue)){
+                setIsEmailInputValid(true);
+                setIsEmailErrorShown(false);
+               } else {
+                setIsEmailInputValid(false);
+                setIsEmailErrorShown(true);
+               }
+        }
+    }
+    const validatePass = () => {
+        if(passInputRef.current) {
+            const passValue = passInputRef.current.value;
+           if(passRegex.test(passValue)){
+            setIsPassInputValid(true);
+            setIsPassErrorShown(false);
+           } else {
+            setIsPassInputValid(false);
+            setIsPassErrorShown(true);
+           }
+        }
+        
+    }
     return (
         <>
             <h3 className='loginForm__title'>Login</h3>
             <form action="" className='loginForm'>
                 <label className='loginForm__label'>Email</label>
-                {/* loginForm__input--invalid  loginForm__input--valid*/}
-                <input className='loginForm__input' type="email" placeholder='Enter your email' autoComplete="on"/>
-                <p className='loginForm__validation-error-message'></p>
+                <input 
+                    className={
+                        `loginForm__input 
+                        ${isEmailErrorShown 
+                            ? 'loginForm__input--invalid' 
+                            : ''}
+                        ${isEmailInputValid ? 'loginForm__input--valid' : ''}        
+                        `}
+                    onChange={validateEmail} 
+                    ref={emailInputRef} 
+                    type="email" 
+                    placeholder='Enter your email' 
+                    autoComplete="on"
+                />
+                <p className='loginForm__validation-error-message'>{
+                    isEmailErrorShown ? 'Please enter an valid email address' : ''
+                }</p>
                 <label className='loginForm__label'>Password</label>
-                <input className='loginForm__input' type="password" placeholder='Enter your password' autoComplete="on"/>
-                <p className='loginForm__validation-error-message'>dfgd</p>
-                {/* loginForm__button--valid */}
+                <input className={
+                        `loginForm__input 
+                        ${isPassErrorShown 
+                            ? 'loginForm__input--invalid' 
+                            : ''}
+                        ${isPassInputValid ? 'loginForm__input--valid' : ''}    
+                        `}
+                        onChange={validatePass} 
+                        ref={passInputRef} 
+                        type="password" 
+                        placeholder='Enter your password' 
+                        autoComplete="on" 
+                        required 
+                        min="6"
+                />
+                <p className='loginForm__validation-error-message'>{
+                    isPassErrorShown ? 'Min 6 chars long, contain number, upper&lower case char' : ''
+                }</p>
                 <p className='loginForm__response-error-message loginForm__response-error-message--hidden'>Login failed! Please,  check you password and email and try again</p>
-                <button className='loginForm__button' type='button'>Login</button>
+                <button 
+                    className={
+                        `loginForm__button 
+                        ${ isEmailInputValid&&isPassInputValid ? 'loginForm__button--valid' : ''}
+                        `} 
+                    type='button'
+                    disabled={isEmailInputValid&&isPassInputValid? false : true}
+                    onClick={login}
+                    >Login</button>
             </form>
             <p className='loginForm__caption'>Don’t have an account? 
                 <a onClick={() => {setFormToShow('sign-up')}} className='loginForm__caption-link'> Sign up</a>
@@ -25,3 +100,4 @@ const LoginForm = ({setFormToShow}:{setFormToShow:(string:string)=>void}) => {
 }
 
 export {LoginForm};
+
--- final-task/src/components/SignUpForm/SignUpForm.tsx
+++ final-task/src/components/SignUpForm/SignUpForm.tsx
@@ -1,29 +1,136 @@
 import './SignUpForm.css';
+import {useRef, useState} from 'react';
 
 const SignUpForm = ({setFormToShow}:{setFormToShow:(string:string)=>void}) => {
 
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passInputRef = useRef<HTMLInputElement>(null);
+    const passRepeatInputRef = useRef<HTMLInputElement>(null);
+    const [isEmailInputValid, setIsEmailInputValid] = useState(false);
+    const [isPassInputValid, setIsPassInputValid] = useState(false);
+    const [isPassRepeatInputValid, setIsPassRepeatInputValid] = useState(false);
+    const [isEmailErrorShown, setIsEmailErrorShown] = useState(false);
+    const [isPassErrorShown, setIsPassErrorShown] = useState(false);
+    const [isPassRepeatErrorShown, setIsPassRepeatErrorShown] = useState(false);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$/;
+    // TODO mb create functions that returns related error messages. Refactor ugly ternary operators in jsx.
+    const signUp = () => {
+        console.log('SIGN-UP')
+    }
+    const validateEmail = () => {
+        if(emailInputRef.current){
+            const emailValue = emailInputRef.current.value;
+            if(emailRegex.test(emailValue)){
+                setIsEmailInputValid(true);
+                setIsEmailErrorShown(false);
+               } else {
+                setIsEmailInputValid(false);
+                setIsEmailErrorShown(true);
+               }
+        }
+    }
+    const validatePass = () => {
+        if(passInputRef.current) {
+            const passValue = passInputRef.current.value;
+           if(passRegex.test(passValue)){
+            setIsPassInputValid(true);
+            setIsPassErrorShown(false);
+            validateRepeatPass()
+           } else {
+            setIsPassInputValid(false);
+            setIsPassErrorShown(true);
+           }
+        }
+        
+    }
+    const validateRepeatPass = () => {
+        if(passInputRef.current && passRepeatInputRef.current){
+            const passValue = passInputRef.current.value;
+            const passRepeatValue = passRepeatInputRef.current.value;
+            if(passValue !== passRepeatValue) {
+                setIsPassRepeatInputValid(false);
+                setIsPassRepeatErrorShown(true);
+            } else {
+                setIsPassRepeatInputValid(true);
+                setIsPassRepeatErrorShown(false);
+            }
+        }
+    }
     return (
         <>
             <h3 className='SignUpForm__title'>Sign up</h3>
             <form action="" className='SignUpForm'>
                 <label className='SignUpForm__label'>Email</label>
-                {/* SignUpForm__input--invalid  SignUpForm__input--valid*/}
-                <input className='SignUpForm__input' type="email" placeholder='Enter your email' autoComplete="on"/>
-                <p className='SignUpForm__validation-error-message'></p>
+                <input 
+                    className={
+                    `SignUpForm__input 
+                    ${isEmailErrorShown ? 'SignUpForm__input--invalid' : ''} 
+                    ${isEmailInputValid ? 'SignUpForm__input--valid' : ''}
+                    `} 
+                    type="email" 
+                    placeholder='Enter your email' 
+                    autoComplete="on" 
+                    ref={emailInputRef}
+                    onChange={validateEmail}
+                />
+                <p className='SignUpForm__validation-error-message'>
+                    {isEmailErrorShown ? 'Please enter an valid email address' : ''}
+                </p>
                 <label className='SignUpForm__label'>Password</label>
-                <input className='SignUpForm__input' type="password" placeholder='Enter your password' autoComplete="on"/>
-                <p className='SignUpForm__validation-error-message'>dfgd</p>
+                <input 
+                    className={
+                        `SignUpForm__input 
+                        ${isPassErrorShown ? 'SignUpForm__input--invalid' : ''} 
+                        ${isPassInputValid ? 'SignUpForm__input--valid' : ''}
+                        `} 
+                    type="password" 
+                    placeholder='Enter your password' 
+                    autoComplete="on" 
+                    min="6" 
+                    required 
+                    ref={passInputRef}
+                    onChange={validatePass}
+                />
+                <p className='SignUpForm__validation-error-message'>
+                    {isPassErrorShown ? 'Min 6 chars long, contain number, upper&lower case char' : ''}
+                </p>
                 <label className='SignUpForm__label'>Repeat Password</label>
-                <input className='SignUpForm__input' type="password" placeholder='Enter your password again' autoComplete="on"/>
-                <p className='SignUpForm__validation-error-message'></p>
-                {/* SignUpForm__button--valid */}
+                <input 
+                    className={
+                        `SignUpForm__input 
+                        ${isPassRepeatErrorShown ? 'SignUpForm__input--invalid' : ''} 
+                        ${isPassRepeatInputValid ? 'SignUpForm__input--valid' : ''}
+                        `}  
+                    type="password" 
+                    placeholder='Enter your password again' 
+                    autoComplete="on"
+                    required
+                    min='6'
+                    ref={passRepeatInputRef}
+                    onChange={validateRepeatPass}
+                />
+                <p className='SignUpForm__validation-error-message'>
+                    {isPassRepeatErrorShown ? 'Passwords do not match' : ''}
+                </p>
                 <p className='SignUpForm__response-error-message SignUpForm__response-error-message--hidden'>Login failed! Please,  check you password and email and try again</p>
-                <button className='SignUpForm__button' type='button'>Create Account</button>
+                <button 
+                    className={`
+                        SignUpForm__button
+                        ${ isEmailInputValid&&isPassInputValid&&isPassRepeatInputValid ? 'SignUpForm__button--valid' : ''}
+                    `} 
+                    type='button' 
+                    disabled={isEmailInputValid&&isPassInputValid&&isPassRepeatInputValid ? false : true}
+                    onClick={signUp}
+                >
+                    Create Account
+                </button>
             </form>
             <p className='SignUpForm__caption'>Already have an account?  
                 <a onClick={() => {setFormToShow('login')}} className='SignUpForm__caption-link'> Login</a>
             </p>
         </>
+        
     )
 }
 
